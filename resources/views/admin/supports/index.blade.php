45f8f490c008a674859440fefcb36cb2e58e1c65
Kelvin Medeiros <kelvindk9@gmail.com>
2024-02-03 21:53:05 -0300

    creade layout list support

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,37 +1,17 @@
-<h1>Listagem dos Suportes</h1>
+@extends('admin.templates.app')
 
-<a href="{{ route('supports.create') }}">Novo suporte</a>
+@section('title', 'Fórum')
 
-<br><br>
+@section('header')
+@include('admin.supports.partials.header', compact('supports'))
+@endsection
 
-<table>
-    <thead>
-        <th>Assunto</th>
-        <th>Status</th>
-        <th>Descrição</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href=" {{ route('supports.show', $support->id) }} ">
-                        Visualizar
-                    </a>
-                     |
-                    <a href=" {{ route('supports.edit', $support->id) }} ">
-                        Editar
-                    </a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('content')
+
+@include('admin.supports.partials.content', compact('supports'))
 
 <x-pagination
     :paginator="$supports"
     :appends="$filters"
 />|
+@endsection
